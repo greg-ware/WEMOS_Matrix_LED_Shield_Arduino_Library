0900118e04439ccd8ea1acf46b055d76ba3e706b
greg-ware <greg.ware@laposte.net>
2017-12-13 23:15:57 +0100

    Declare ssid and password vars

--- examples/5x5characters/D1Mini_banner_MQTT.cpp
+++ examples/5x5characters/D1Mini_banner_MQTT.cpp
@@ -32,6 +32,8 @@ MLED mled(5); //set intensity=5
 
 // Update these with values suitable for your network.
 
+const char* ssid = "-";
+const char* password = "";
 const char* mqtt_server =  "192.168.1.199";
 
 WiFiClient espClient;
